e8e25f7c4866c6e2b071d8ae9032a105cd4a36ea
Mattia Robbiano <mattia@DebianMattia.homenet.telecomitalia.it>
2024-01-21 19:13:04 +0100

    aggiornato notebook con spiegazioni

--- quantum.py
+++ quantum.py
@@ -2,50 +2,57 @@ from qiskit import ClassicalRegister, QuantumRegister
 from qiskit import QuantumCircuit
 from qiskit.circuit.library import RealAmplitudes
 from qiskit.quantum_info import Statevector
-#from qiskit.utils import algorithm_globals
 
+#Funzione che prende in input il numero di qubit e la profondità del circuito e restituisce il circuito ansatz
+#patametrico di tipo RealAmplitudes corrispondente.
 def AnsatzBuilder(QubitNumber, Depth):
     return RealAmplitudes(QubitNumber, reps=Depth)
 
+#Funzione che prende in input il numero di qubit nel TrashSpace e restituisce il circuito SwapTest corrispondente.
 def SwaptestBuilder(TrashSpaceDimension):
-    QubitNumber = 2*TrashSpaceDimension + 1
+    QubitNumber = 2*TrashSpaceDimension + 1 #calcolo il numero di qubit totali necessari
+    #Inizializzo i registri quantistici e classici
     QuantReg = QuantumRegister(QubitNumber,"q")
     ClassReg = ClassicalRegister(1,"c")
+    #Inizializzo il circuito SwapTest
     SwaptestCircuit = QuantumCircuit(QuantReg, ClassReg)
-    AuxiliaryQubitLable = QubitNumber-1
+    AuxiliaryQubitLable = QubitNumber-1 #Label dell'ultimo qubit (quello ausiliario)
     
-    #INITIALIZE REFERENCE QUBITS TO 0
+    #Inizializzo i qubit nel ReferenceSpace a 0
     for i in range(TrashSpaceDimension,2*TrashSpaceDimension): SwaptestCircuit.reset(i)
 
-    #H GATE ON AUXILIARY QUBIT
-    SwaptestCircuit.h(AuxiliaryQubitLable)
-    #CSWAP GATES BETWEEN TRASH AND REFERENCE QUBITS TARGETING AUXILIARY QUBIT
+    #Costruisco il circuito SwapTest
+
+    SwaptestCircuit.h(AuxiliaryQubitLable) #porta H sul qubit ausiliario
+    #CSWAP gates tra Trash e Reference qubits targeting Auxiliary qubit
     for i in range(TrashSpaceDimension):
         SwaptestCircuit.cswap(AuxiliaryQubitLable, i, TrashSpaceDimension+i)
-    #H GATE ON AUXILIARY QUBIT
-    SwaptestCircuit.h(AuxiliaryQubitLable)
-    SwaptestCircuit.measure(AuxiliaryQubitLable, ClassReg[0])
+
+    SwaptestCircuit.h(AuxiliaryQubitLable) #porta H sul qubit ausiliario
+    SwaptestCircuit.measure(AuxiliaryQubitLable, ClassReg[0]) #misura il qubit ausiliario
 
     return SwaptestCircuit
 
+#Funzione che prende in input la dimensione dello spazio di input, la dimensione dello spazio di output e la profondità
+#del circuito ansatz e restituisce il circuito Encoder completo (usando SwaptestBuilder e AnsatzBuilder)
 def EncoderBuilder(InputStateDimension, EncodedStateDimension, Depth):
-    #Calculate dimension of spaces
+    #Calcolo le dimensioni dei vari spazi
     LatentSpaceDimension = EncodedStateDimension
     TrashSpaceDimension = InputStateDimension - EncodedStateDimension
     ReferenceSpaceDimension = TrashSpaceDimension
     TotalQubitNumber = LatentSpaceDimension + TrashSpaceDimension + ReferenceSpaceDimension +1 #+1 (for auxiliary qubit)
 
-    #Initialize circuit
+    #Inizializzo i registri quantistici e classici
     QuantReg = QuantumRegister(TotalQubitNumber,"q")
     ClassReg = ClassicalRegister(1,"c")
     Circuit = QuantumCircuit(QuantReg, ClassReg)
 
-    #Add AnsatzCircuit
+    #Aggiungo AnsatzCircuit
     Ansatz = AnsatzBuilder(LatentSpaceDimension+TrashSpaceDimension, Depth)
     Circuit.compose(Ansatz,range(0, InputStateDimension), inplace=True)
-    #Barrier to define optimization regions
+    #Barriera per delimitare i vari spazi
     Circuit.barrier()
-    #Add SwapTestCircuit
+    #Aggiungo SwapTestCircuit
     Circuit.compose(SwaptestBuilder(TrashSpaceDimension),range(LatentSpaceDimension, TotalQubitNumber), inplace=True)
     
     return Circuit
